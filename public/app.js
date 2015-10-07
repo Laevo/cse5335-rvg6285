2233907c1145ae558f9b2b840834ede2ceaabf77
Laevo <rohitvgaikwad@mavs.uta.edu>
2015-10-06 21:48:41 -0500

    df

--- public/app.js
+++ public/app.js
@@ -1,19 +1,8 @@
 var app = angular.module('myTestApp', []);
-
-
 app.controller('myController', ['$scope', '$http', function($scope, $http) {
-
-
   $scope.clickButton = function() {
-
     $http.get('test.json').success(function(data) {
-
         $scope.message = data.msg;
-
       });
-
   }
-
-  
-
 }]);
